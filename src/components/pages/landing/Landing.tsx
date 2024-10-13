9a6ad21c5b099ec9a80742227c09290ea9dac30b
mosefatullah <mosefatullah@gmail.com>
2024-10-13 11:45:59 +0600

    .

--- src/components/pages/landing/Landing.tsx
+++ src/components/pages/landing/Landing.tsx
@@ -2,19 +2,53 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useTranslation } from "react-i18next";
 import { Helmet } from "react-helmet";
+import { langType } from "@/lib/constants";
+import { Link } from "react-router-dom";
 
 export const Landing = () => {
- const { t } = useTranslation();
+ const { t, i18n } = useTranslation();
+
+ const courses = [
+  {
+   cover: "sohoje_spoken_arbi_16x9.jpg",
+   link: "easy-spoken-arabic",
+   type: "course",
+   en: {
+    title: "Easy Spoken Arabic",
+    instructor: "Mahade Hasan",
+    lang: "Bengali",
+    price: "3000",
+    reducedPrice: "2000",
+   },
+   bn: {
+    title: "সহজ কথিত আরবি",
+    instructor: "মাহাদি হাসান",
+    lang: "বাংলা",
+    price: "৩০০০",
+    reducedPrice: "২০০০",
+   },
+   ar: {
+    title: "العربية المنطوقة بسهولة",
+    instructor: "مهاد حسن",
+    lang: "البنغالية",
+    price: "٣ ٠ ٠ ٠",
+    reducedPrice: "٢ ٠ ٠ ٠",
+   },
+  },
+ ];
+
  return (
   <>
-  <Helmet>
+   <Helmet>
     <title>{t("landing.main.meta.title")}</title>
     <meta name="description" content={t("landing.main.meta.description")} />
-  </Helmet>
+   </Helmet>
    <div className="grid lg:grid-cols-2 px-8 py-24 items-center max-w-7xl mx-auto">
     <div>
      <h1 className="text-4xl font-bold">{t("landing.main.hero.title")}</h1>
-     <p className="text-lg mt-6 text-gray-500">{t("landing.main.hero.subtitle")}</p>
+     <p className="text-lg mt-6 text-gray-500">
+      {t("landing.main.hero.subtitle")}
+     </p>
      <Button className="mt-6">{t("landing.main.hero.button")}</Button>
     </div>
 
@@ -27,8 +61,12 @@ export const Landing = () => {
       />
       <div className="w-full bg-secondary/50 p-4 rounded-lg mt-4 border border-input">
        <div className="min-h-[200px]">
-        <h3 className="text-xl font-semibold">{t("landing.main.hero.search.title")}</h3>
-        <p className="text-gray-500 text-sm mt-2">{t("landing.main.hero.search.null")}</p>
+        <h3 className="text-xl font-semibold">
+         {t("landing.main.hero.search.title")}
+        </h3>
+        <p className="text-gray-500 text-sm mt-2">
+         {t("landing.main.hero.search.null")}
+        </p>
        </div>
       </div>
      </div>
@@ -38,22 +76,53 @@ export const Landing = () => {
    <div className="bg-primary text-primary-foreground px-8 py-14 rounded-t-3xl">
     <div className="text-center max-w-2xl mx-auto">
      <h1 className="text-4xl font-bold">{t("landing.main.courses.title")}</h1>
-     <p className="text-lg mt-4 text-gray-400">{t("landing.main.courses.subtitle")}</p>
+     <p className="text-lg mt-4 text-gray-400">
+      {t("landing.main.courses.subtitle")}
+     </p>
     </div>
 
     <div className="grid grid-cols-3 gap-4 mt-12 max-w-5xl mx-auto">
-     <div>
-      <img
-       src={`${import.meta.env.BASE_URL}uploads/sohoje_spoken_arbi_16x9.jpg`}
-       alt="couimport Navbar from './components/Navbar';
-rse"
-       className="w-full object-cover rounded-t-lg aspect-w-16 aspect-h-9"
-      />
-      <div className="bg-secondary text-secondary-foreground p-4 rounded-b-lg">
-       <h3 className="text-xl font-semibold">Easy Spoken Arabic</h3>
-       <p className="text-gray-500 text-sm mt-2">Mahade Hasan</p>
-      </div>
-     </div>
+     {courses.map((course, index) => (
+      <Link key={index} to={`/product/${course.type}::${course.link}`}>
+       <div className="!max-w-[300px] cursor-pointer hover:transform hover:scale-105 transition-transform">
+        <img
+         src={`${import.meta.env.BASE_URL}uploads/${course.cover}`}
+         alt="course"
+         className="w-full object-cover rounded-t-lg aspect-w-16 aspect-h-9"
+        />
+        <div className="bg-secondary text-secondary-foreground p-3 rounded-b-lg">
+         <h3 className="font-semibold">
+          {course[i18n.language as langType].title}
+         </h3>
+         <p className="text-gray-500 text-xs">
+          {course[i18n.language as langType].instructor}
+         </p>
+         <div className="flex items-center justify-between gap-2">
+          <span className="text-xs mt-2 inline-block bg-primary text-primary-foreground px-2 py-1 rounded">
+           {course[i18n.language as langType].lang}
+          </span>
+          <div
+           className={
+            "price mt-2 flex gap-2 items-center" +
+            (i18n.language === "ar" ? " flex-row-reverse tracking-tighter" : "")
+           }
+          >
+           <p className="text-sm text-gray-500 line-through ml-2">
+            {i18n.language === "ar"
+             ? course.ar.price + " ৳"
+             : "৳" + course[i18n.language as langType].price}
+           </p>
+           <p className="text-md font-semibold">
+            {i18n.language === "ar"
+             ? course.ar.reducedPrice + " ৳"
+             : "৳" + course[i18n.language as langType].reducedPrice}
+           </p>
+          </div>
+         </div>
+        </div>
+       </div>
+      </Link>
+     ))}
     </div>
    </div>
   </>
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,7 @@
 export const LANGUAGES = [
  { label: "العربية", code: "ar" },
- { label: "English", code: "en" },
  { label: "বাংলা", code: "bn" },
+ { label: "English", code: "en" },
 ];
+
+export type langType = "ar" | "bn" | "en";
